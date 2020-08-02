281074ae75f8ef36a91decb7ef60136b59de8b2e
Zijian_Ou <kenou1996@126.com>
2020-08-02 23:13:45 +0800

    [Second] Typo

--- second_stage/reduction.h
+++ second_stage/reduction.h
@@ -9,6 +9,6 @@ void reduction_setup(int batch_size, int tuple_size);
 
 void reduction_process(batch_p batch, int tuple_size, int qid, batch_p output);
 
-void reduction_print_output(batch_p output, int batch_size, int tuple_size);
+void reduction_print_output(batch_p outputs, int batch_size, int tuple_size);
 
 #endif
