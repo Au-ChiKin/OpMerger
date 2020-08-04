5b5366775ef4eed14f6e3b854ecdcc59d1ebd75f
Zijian_Ou <kenou1996@126.com>
2020-08-05 01:17:02 +0800

    [Second] Specify usage of this struct

--- second_stage/batch.h
+++ second_stage/batch.h
@@ -6,8 +6,8 @@
 typedef struct batch * batch_p;
 typedef struct batch {
     long start;
-    long end;
-    int size;   /* size in tuples */
+    long end;   /* end - start gives the allocated size of this buffer */
+    int size;   /* variable, in tuples, marking the number of stored meaning tuples */
     u_int8_t * buffer;
 } batch_t;
 
