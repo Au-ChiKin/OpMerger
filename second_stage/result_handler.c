ddb68fa418f82d2d543a20713bc2140052823c7c
Zijian_Ou <kenou1996@126.com>
2020-08-23 12:03:55 +0800

    [Second] Use event_manager as base for result_handler

--- second_stage/result_handler.c
+++ second_stage/result_handler.c
@@ -3,13 +3,103 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static pthread_t thr = NULL;
 
-result_handler_p result_handler() {
-	result_handler_p p = (result_handler_p) malloc(sizeof(result_handler_t));
+static void process_one_event (result_handler_p m);
+static void reset_data(result_handler_p p);
 
+static void * result_handler(void * args) {
+	result_handler_p p = (result_handler_p) args;
+
+	/* Unblocks the thread (which runs result_handler_init) waiting for this thread to attach to the JVM */
+	p->start = 1;
+
+    while (1) {
+        pthread_mutex_lock (p->mutex);
+            while (p->event_tail - p->event_head == 0) {
+                pthread_cond_wait(p->added, p->mutex);
+            }
+
+                process_one_event(p);
+        pthread_mutex_unlock (p->mutex);
+    }
+
+	return (args) ? NULL : args;
+}
+
+result_handler_p result_handler_init() {
+
+	result_handler_p p = (result_handler_p) malloc (sizeof(result_handler_t));
+	if (! p) {
+		fprintf(stderr, "fatal error: out of memory\n");
+		exit(1);
+	}
+
+    p->start = 0;
+
+    p->event_head = 0;
+    p->event_tail = 0;
+    for (int i=0; i<RESULT_HANDLER_QUEUE_LIMIT; i++) {
+        p->events[i] = NULL;
+    }
+
+    /* Accumulated data */
+    reset_data(p);
+
+	/* Initialise mutex and conditions */
+	p->mutex = (pthread_mutex_t *) malloc (sizeof(pthread_mutex_t));
+	pthread_mutex_init (p->mutex, NULL);
+
+	p->added = (pthread_cond_t *) malloc (sizeof(pthread_cond_t));
+	pthread_cond_init (p->added, NULL);
+
+	/* Initialise thread */
+	if (pthread_create(&thr, NULL, result_handler, (void *) p)) {
+		fprintf(stderr, "error: failed to create throughput monitor thread\n");
+		exit (1);
+	}
+	/* Wait until thread attaches itself to the JVM */
+	while (! p->start)
+		;
 	return p;
 }
 
-void result_handler_add_task (result_handler_p p, task_p t) {
-
+void result_handler_add_event (result_handler_p p, query_event_p e) {
+	pthread_mutex_lock (p->mutex);
+        if (p->events[p->event_tail] != NULL) {
+            free(p->events[p->event_tail]);
+            p->events[p->event_tail] = NULL;
+        }
+    	p->events[p->event_tail] = e;
+        p->event_tail = (p->event_tail + 1) % RESULT_HANDLER_QUEUE_LIMIT;
+    pthread_mutex_unlock (p->mutex);
+	
+    pthread_cond_signal (p->added);
 }
+
+void result_handler_get_data (result_handler_p p, int * event_num, long * processed_data, long * latency_sum) {
+    pthread_mutex_lock (p->mutex);
+        *event_num = p->event_num;
+        *processed_data = p->processed_data;
+        *latency_sum = p->latency_sum;
+
+        reset_data(p);
+    pthread_mutex_unlock (p->mutex);
+}
+
+static void process_one_event (result_handler_p p) {
+    query_event_p e = p->events[p->event_head];
+
+    p->event_num += 1;
+    p->processed_data += e->tuples * e->tuple_size;
+    p->latency_sum += e->end - e->start;
+
+    p->event_head = (p->event_head + 1) % RESULT_HANDLER_QUEUE_LIMIT;
+}
+
+static void reset_data(result_handler_p p) {
+    p->event_num = 0;
+    p->latency_sum = 0;
+    p->processed_data = 0;
+}
+
--- second_stage/result_handler.h
+++ second_stage/result_handler.h
@@ -1,10 +1,20 @@
 #ifndef __RESULT_HANDLER_H_
 #define __RESULT_HANDLER_H_
 
-#include "task.h"
+#include <pthread.h>
+
+#define RESULT_HANDLER_QUEUE_LIMIT 100
 
 typedef struct result_handler * result_handler_p;
 typedef struct result_handler {
+    pthread_mutex_t * mutex;
+    pthread_cond_t * added;
+
+    volatile unsigned start;
+
+    volatile int event_head;
+    volatile int event_tail;
+    volatile query_event_p events [RESULT_HANDLER_QUEUE_LIMIT];
 
     /* Accumulated data */
     volatile int event_num;
@@ -12,8 +22,10 @@ typedef struct result_handler {
     volatile long latency_sum;
 } result_handler_t;
 
-result_handler_p result_handler();
+result_handler_p result_handler_init();
 
-void result_handler_add_task (result_handler_p p, task_p t);
+void result_handler_add_event (result_handler_p p, query_event_p e);
+
+void result_handler_get_data (result_handler_p p, int * event_num, long * processed_data, long * latency_sum);
 
 #endif
