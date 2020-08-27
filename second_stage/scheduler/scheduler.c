4eaae9b68e010a0e454a329a1f822ecf0133b1a3
Zijian_Ou <kenou1996@126.com>
2020-08-28 01:53:34 +0800

    [Second] Try to pin a core

--- second_stage/scheduler/scheduler.c
+++ second_stage/scheduler/scheduler.c
@@ -15,6 +15,17 @@ static task_p take_one_task(scheduler_p p);
 static void * scheduler(void * args) {
 	scheduler_p p = (scheduler_p) args;
 
+#ifndef __APPLE__
+	/* Pin this thread to a particular core: 0 is the dispatcher, 1 is the GPU */
+	int core = 1;
+	cpu_set_t set;
+	CPU_ZERO (&set);
+	CPU_SET (core, &set);
+	sched_setaffinity (0, sizeof(set), &set);
+	fprintf(stdout, "[DBG] result handler attached to core 2\n");
+	fflush (stdout);
+#endif
+
 	/* Unblocks the thread (which runs scheduler_init) waiting for this thread to start */
 	p->start = 1;
 
